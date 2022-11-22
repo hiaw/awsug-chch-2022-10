03da9da53044c9240f18caecf40c6bb12b80aac3
Daniel Chong <danielgq.chong@gmail.com>
2022-11-22 21:40:21 +1300

    remove auth and bucket demo

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -57,9 +57,7 @@ export default function Home() {
         <Empty>&#10024; Post the first link &#10024;</Empty>
       )}
       <div className={styles.row}>
-        <Folding name="Auth">Auth</Folding>
         <Folding name="Secret">{secret.data?.secret.key}</Folding>
-        <Folding name="Bucket">Secret</Folding>
       </div>
     </div>
   );
